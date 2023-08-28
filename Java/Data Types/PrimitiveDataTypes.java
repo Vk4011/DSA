3f00c7db413078add4091a8b890a5e2fe661d117
Vk4011 <maticsystem1@gmail.com>
2023-08-28 20:43:25 +0530

    Primitive Data Types

---
+++ Java/Data Types/PrimitiveDataTypes.java
@@ -0,0 +1,40 @@
+public class PrimitiveDataTypes{
+     public static void main(String[] args) {
+            System.out.println("\n\t Primitive Data Types \n");
+            byte b=8;
+            System.out.println("\n\t"+b);
+            char ch='a';
+            System.out.println("\n\t"+ch);
+            boolean var = false;
+            float price = 10.5f;
+            int number = 25;
+            short n = 240;
+            long l=1023456789;
+            double decimal = 3.145678;
+            System.out.println("\n\t"+var);
+            System.out.println("\n\t"+price);
+            System.out.println("\n\t"+number);
+            System.out.println("\n\t"+n);
+            System.out.println("\n\t"+decimal);
+            System.out.println("\n\t"+l);
+
+
+    }
+}
+
+
+//Primitive Data types in java
+/*
+ * 
+ byte
+ short 
+ char 
+ boolean 
+ int
+ long 
+ float 
+ double
+ 
+
+
+ */
