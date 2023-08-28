76eeed2fd28061e6f606f188c721cf6983f4ce3f
Vk4011 <maticsystem1@gmail.com>
2023-08-28 20:52:28 +0530

    Non Permitive Data types

---
+++ Java/Data Types/Non.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class Non {
+    public static void main(String[] args) {
+        System.out.println("\n\t Enter the String : ");      
+        Scanner sc = new Scanner(System.in);
+        String str=sc.next();
+        System.out.println("\n\t string value :"+str);
+        String line = sc.nextLine();
+        System.out.println("\n\t Complete Paragraph :"+line); 
+          
+    }
+}
+//Output : 
+// Enter the String :
+// Hello world i am Robo
+
+//          string value :Hello
+
+//          Complete Paragraph : world i am Robo
+
+
