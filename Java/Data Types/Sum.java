a136654a3da525f8f0b5c9494dbeb8097dbf75b1
Vk4011 <maticsystem1@gmail.com>
2023-08-29 11:06:01 +0530

    Sum

---
+++ Java/Data Types/Sum.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class Sum {
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("\n\t Sum of Two Nums");
+    int a=input.nextInt();
+    System.out.println("\n\t Enter a value :"+a);
+    int b=input.nextInt();
+    System.out.println("\n\t Enter b value :"+b);
+    int sum = a+b;
+    System.out.println("\n\t Sum of two nums : "+sum);
+
+}
+    
+}
