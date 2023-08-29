b869069e9a1b4907a52e8c61fb448f4252b2075c
Vk4011 <maticsystem1@gmail.com>
2023-08-29 11:47:55 +0530

    Logical Operator

---
+++ Java/Operators/Logical.java
@@ -0,0 +1,24 @@
+public class Logical {
+    public static void main(String[] args) {
+        System.out.println("\n\t Logical AND && ");
+        System.out.println((2>3)&&(4<1));
+        System.out.println("\n\t Logical OR ||  ");
+        System.out.println((2<3)||(4<1));
+         System.out.println("\n\t Logical NOT !  ");
+         System.out.println(!(3>2));
+    }
+    
+}
+
+// output:
+
+//          Logical AND &&
+// false
+
+//          Logical OR ||
+// true
+
+//          Logical NOT !
+// false
+
+
