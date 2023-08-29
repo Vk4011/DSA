4f2c552cbcee7fbd9f54fa31d52eb7ddff737285
Vk4011 <maticsystem1@gmail.com>
2023-08-29 11:24:41 +0530

    Yidenying conversion

---
+++ Java/Type Casting/Implicit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Implicit{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n\t implicit conversion ");
+        System.out.println("\n\t Enter a value :");
+        float Number = input.nextInt();
+        System.out.println(Number);
+
+    }
+}
+=
