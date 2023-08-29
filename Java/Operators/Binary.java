943d194b4210fea353722539560e6742ed6ff5a8
Vk4011 <maticsystem1@gmail.com>
2023-08-29 11:29:11 +0530

    Binary operators

---
+++ Java/Operators/Binary.java
@@ -0,0 +1,11 @@
+public class Binary {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=a*b;
+        System.out.println(c);
+    }
+}
+
+// output : 200
+// binary operator in java consist two operands
