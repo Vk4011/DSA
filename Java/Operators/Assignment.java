aef2ea119ebc5ce4b0f6355f668bf385ca287000
Vk4011 <maticsystem1@gmail.com>
2023-08-29 11:42:02 +0530

    Relational Operator

---
+++ Java/Operators/Assignment.java
@@ -0,0 +1,21 @@
+public class Assignment {
+    public static void main(String[] args) {
+        int a=7;
+        a*=5;
+        System.out.println(a);
+        a-=5;
+        System.out.println(a);
+        a+=5;
+        System.out.println(a);
+        a/=7;
+        System.out.println(a);
+    }
+}
+
+//  output :
+// 35
+// 30
+// 35
+// 5
+
+
