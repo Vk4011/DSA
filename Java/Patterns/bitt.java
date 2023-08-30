510352c9c293c1a1d4a1978a5874164c9895e20a
Vk4011 <maticsystem1@gmail.com>
2023-08-30 11:00:39 +0530

    01 Triangle

---
+++ Java/Patterns/bitt.java
@@ -0,0 +1,43 @@
+public class bitt {
+    public static void Zero_One_Triangle(int n){
+        System.out.println("\n\t Zero One Triangle \n");
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("\t1");//Even
+
+                }else{
+                    System.out.print("\t0");
+                }
+
+            }
+            System.out.println("\n");
+        }
+    }
+    public static void main(String[] args) {
+        Zero_One_Triangle(5);
+    }
+    
+}
+
+
+
+
+// output:
+//  Zero One Triangle
+
+//         1
+
+//         0       1
+
+//         1       0       1
+
+//         0       1       0       1
+
+//         1       0       1       0       1
+
+//         0       1       0       1       0       1
+
+
+
+.
