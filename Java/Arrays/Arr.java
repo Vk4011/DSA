0b245e1cb7544869499af85c5af5a344143e9cb2
Vk4011 <maticsystem1@gmail.com>
2023-09-01 19:04:50 +0530

    Arrays

---
+++ Java/Arrays/Arr.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class Arr {
+    public static void main(String[] args) {
+        int arr[] = {12,35,67,89,90};
+        System.out.println("\n\t The Array \n");
+        System.out.println("\n\t Array elements are  : "+Arrays.toString(arr));
+        System.out.println();
+    }    
+}
+
+// output:
+//         The Array
+
+
+//          Array elements are  : [12, 35, 67, 89, 90]
---
+++ Java/Arrays/Basic.java
@@ -0,0 +1,31 @@
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println("\n\t Creating Array \n");
+        int arr[] = new int[50];
+        arr[0]=90;
+        int[]  num ={1,2,4,5,6,77,89};
+        String[] fruits={"apple","mangoe","banna","carrot"};
+        int marks[]={70,80,90};
+        System.out.println("\n\t Array is :"+arr[0]);
+        System.out.println("\n\t Array value at index 10 :"+(arr[10]=89));
+        System.out.println("\n\t Numbers Array is :"+num);
+        System.out.println("\n\t Fruits array  :"+fruits);
+        System.out.print("\n\t Array is :"+marks);
+    }
+    
+}
+
+// output:
+    
+//          Creating Array
+
+
+//          Array is :90
+
+//          Array value at index 10 :89
+
+//          Numbers Array is :[I@1761e840
+
+//          Fruits array  :[Ljava.lang.String;@5ecddf8f
+
+//          Array is :[I@3f102e87
