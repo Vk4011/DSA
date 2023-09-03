53d1c4a848905a6a21198cf4eda4ec9fc7c52fdb
Vk4011 <maticsystem1@gmail.com>
2023-09-03 16:20:40 +0530

    Imutable

---
+++ Java/Strings/Imutable.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Imutable{
+    public static void main(String[] args) {
+        
+    
+    char arr[]={'a','b','c','d','e'};
+    String str="abcd";
+    System.out.println("\n\t Strings are imutable in java\n");
+    Scanner sc=new Scanner(System.in);
+    String name;
+    System.out.println("\n\t Enter a String : ");
+    name=sc.next();
+    System.out.println("\n\n\t String is : "+name);
+
+    
+}
+}
+        output
+
+                Strings are imutable in java
+
+
+         Enter a String : 
+vikram
+
+
+         String is : vikram
