45ad72784aea814199966a1c408378d668cc90ca
Vk4011 <maticsystem1@gmail.com>
2023-09-07 09:43:34 +0530

    Search key

--- Java/2D Arrays/Matrix.java
+++ Java/2D Arrays/Matrix.java
@@ -21,7 +21,54 @@ public class Matrix {
             System.out.println("\n");
         }
         System.out.println("\n");
+        search(Matrix, 5);
 
 
     }
+
+    public static boolean search(int martix[][],int key){
+         for(int i =0;i<martix.length;i++){
+            for(int j=0;j<martix[0].length;j++){
+                if(martix[i][j] == key ){
+                    System.out.println("\n\t found at cell ("+i+","+j+")");
+                    return true;
+                }
+
+            }
+            System.out.println("\n");
+        }
+        System.out.println("\n\t Key not found");
+        return false;
+        
+    }
 }
+                     Matrix
+
+         Enter the values :
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+         Output :
+1 2 3
+
+4 5 6
+
+7 8 9
+
+
+
+
+         found at cell (1,1)
+
+
+
+
+         
