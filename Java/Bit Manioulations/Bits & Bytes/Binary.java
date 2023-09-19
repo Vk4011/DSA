f0b9cbe61c2bc0b40ed67ce74cfddecaae31751d
Vk4011 <maticsystem1@gmail.com>
2023-09-19 17:15:05 +0530

    Left Shif <<

---
+++ Java/Bit Manioulations/Bits & Bytes/Binary.java
@@ -0,0 +1,31 @@
+public class Binary {
+    public static void main(String[] args) {
+        System.out.println("\n\t Binary Left and Right Shift \n");
+        System.out.println("\n\t Formula a<<b = a*2^b \n");
+        System.out.println("\n\t The number a is shifted to left by b places _ _ <== \n");
+        System.out.println("\n\t 5 = (101) ==> ([00]0 101) \n");
+        System.out.println("\n\t Left Shift 5<<2 : "+(5<<2));
+        System.out.println("\n\t =>(0 1 0 1 [0 0]) \n");
+
+    }
+}
+
+            output :
+
+            
+         Binary Left and Right Shift
+
+
+         Formula a<<b = a*2^b
+
+
+         The number a is shifted to left by b places _ _ <==
+
+
+         5 = (101) ==> ([00]0 101)
+
+
+         Left Shift 5<<2 : 20
+
+         =>(0 1 0 1 [0 0])
+
