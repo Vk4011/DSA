b1633d3da657fc36bd13eadae88e1942237ddee1
Vk4011 <maticsystem1@gmail.com>
2023-09-19 17:59:47 +0530

    Binary Right Shift >>

---
+++ Java/Bit Manioulations/Bits & Bytes/Shif.java
@@ -0,0 +1,21 @@
+public class Shif {
+    public static void main(String[] args) {
+        System.out.println("\n\t Right Shift \n");
+        System.out.println("\n\t 6=110 => (000110) => 00011 :  "+(6>>1));
+        System.out.println("\n\t LSB = Right most Significant Bit \n");
+        System.out.println("\n\t Formula : a>>b = a/2^b => 6>>1 = 6/2^1 = 3 // \n");
+
+    }
+    
+}
+            output:
+
+            Right Shift 
+
+
+         6=110 => (000110) => 00011 :  3
+
+         LSB = Right most Significant Bit
+
+
+         Formula : a>>b = a/2^b => 6>>1 = 6/2^1 = 3 //
