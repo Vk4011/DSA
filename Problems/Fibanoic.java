db34a5b5b5caa0d943389b986029719108cb8afd
Vk4011 <maticsystem1@gmail.com>
2023-10-02 19:28:19 +0530

    Fibanoic digit

--- Problems/Fibanoic.java
+++ Problems/Fibanoic.java
@@ -19,6 +19,48 @@ public class Fibanoic {
         for(int i=0;i<n;i++){
             System.out.println(f[i]+" ");
         }
-
+        System.out.println("\n\t Fibonic series method 2\n");
+        int result=fib(n);
+        System.out.println("\n\t Fibonanic  digit : "+result);
     }
-}
+    private static int fib(int x){
+        int f=0;
+        int s=1;
+        if(x<0){
+            System.out.println("\n\t -negative value \n");
+        }
+        for(int i=2;i<x;i++){
+        int r=f+s;
+        f=s;
+        s=r;
+        }
+        return s;
+    }
+}       
+
+
+            output:
+
+
+            
+         Enter a number :
+5
+
+         Fibonacci Series :
+0 
+1
+1 
+2
+3
+
+         Fibonic series method 2
+
+
+         Fibonanic  digit : 3
+
+
+
+
+
+
+         
