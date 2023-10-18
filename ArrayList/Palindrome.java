668e50c8bf978408c2039ce2d0f41e971e2b5bb4
Vk4011 <maticsystem1@gmail.com>
2023-10-18 17:43:43 +0530

    palin List

--- ArrayList/Palindrome.java
+++ ArrayList/Palindrome.java
@@ -1,21 +1,25 @@
-import java.util.*;
-    class Palindrome
-    {
-      public static void main(String[] args) 
-      {
-        ArrayList< String > words = new ArrayList< String >();
-        Scanner scan = new Scanner(System.in);    
-        while(scan.hasNext())     
-           words.add(scan.next());
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Palindrome {
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<String>();
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext())
+            words.add(scan.next());
         System.out.println("Is it a palindrome? " +
-              isPalindrome(words));
-      }
-      static boolean isPalindrome(ArrayList< String > w)
-      {
-       int front  = 0, back = w.size() - 1;
-       while( front < back)
-         if (!w.get(front++).equals(w.get(back--)))
-            return false;
-        return true;
-      }
+                isPalindrome(words));
     }
+
+    static boolean isPalindrome(ArrayList<String> words) {
+        int front = 0, back = words.size() - 1;
+        while (front < back) {
+            if ( !words.get(front).equals(words.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+}
