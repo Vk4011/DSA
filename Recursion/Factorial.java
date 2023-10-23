da2035f5304b6bc297e359f2e08d2b36d90c5a74
Vk4011 <maticsystem1@gmail.com>
2023-10-23 13:13:25 +0530

    factorial

---
+++ Recursion/Factorial.java
@@ -0,0 +1,26 @@
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println("\n\t Factorial of a number \n");
+        int n=5;
+        // fact(n);
+        System.out.println("\n\t factorial of "+n+"! : "+fact(n));
+    }
+    public static int fact(int n) {
+        if(n == 0){
+
+            return 1;
+        }
+        int fn = n * fact(n-1);
+        System.out.print(" * "+fn+" ");
+        return fn;
+    }
+    
+}
+
+//              Factorial of a number
+
+//  * 1  * 2  * 6  * 24  * 120 
+//          factorial of 5! : 120
+
+
+
