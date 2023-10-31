b733414dfd87cbb65fb045a6503cdcce35b0f283
Vk4011 <maticsystem1@gmail.com>
2023-10-31 17:01:42 +0530

    lastOccurence

--- Recursion/Occurence.java
+++ Recursion/Occurence.java
@@ -1,5 +1,5 @@
 public class Occurence {
-    public static void main(StringReverse[] args) {
+    public static void main(String[] args) {
         System.out.println("\n\t WAF to find the first Occurence of an Element in Array \n");
         int arr[] = {8,3,6,9,5,10,2,5,3};
         System.out.print("\n\t Array : [ ");
@@ -8,6 +8,8 @@ public class Occurence {
         }
         System.out.print(" ]\n");
         System.out.println("\n\t First occurence : "+firstOccurence(arr, 5, 0));
+         System.out.println("\n\t WAF to find the Last Occurence of an Element in Array \n");
+        System.out.println("\n\t last occurence : "+lastOccurence(arr, 5, 0));
     }
     public static int firstOccurence(int arr[],int key,int i){
         //Base Case
@@ -19,6 +21,18 @@ public class Occurence {
         }
         return firstOccurence(arr, key, i+1);
 
+    }
+    public static int lastOccurence(int arr[] , int key , int i){
+        if(i==arr.length){
+            return -1;
+
+        }
+        int isFound = lastOccurence(arr, key, i+1);
+    
+        if(isFound == -1 && arr[i]==key){
+            return i;
+        }
+        return isFound;
     }
 }
 
@@ -35,4 +49,13 @@ public class Occurence {
 
 
 
- 
+        
+        //  WAF to find the Last Occurence of an Element in Array
+
+
+        //  last occurence : 7
+
+
+
+
+        
