f4bbeb60c96934ce36e600b77e87c1853490dc18
Vk4011 <maticsystem1@gmail.com>
2023-11-01 15:44:47 +0530

    Strings

--- Recursion/Problems/Binary.java
+++ Recursion/Problems/Binary.java
@@ -3,11 +3,11 @@ public class Binary {
         System.out.println("\n\t Binary String problem \n");
         System.out.println("\n\t Print all binary strings of size N without consecutive ones \n");
         System.out.println("\n\t Print the binary String pairs \n");
-        pbs(3, 0,new StringBuilder());
-
+        // pbs(3, 0,new StringBuilder());
+        pbs(3, 0,"");
 
     }
-    public static void pbs(int n,int lp,StringBuilder str) {
+    public static void pbs(int n,int lp,String str) {//StringBuilder
        
         //Base case 
 
@@ -17,9 +17,9 @@ public class Binary {
         }
 
         //f
-        pbs(n-1,0,str.append("0"));
+        pbs(n-1,0,str+"0");//str.append("0")
         if(lp==0){
-            pbs(n-1,1,str.append("1"));
+            pbs(n-1,1,str+"1");//str.append("1")
 
         }
 
@@ -41,3 +41,20 @@ public class Binary {
         //  000110
         //  000110100
         //  0001101001
+
+
+        //String 
+
+        //  Binary String problem
+
+
+        //  Print all binary strings of size N without consecutive ones
+
+
+        //  Print the binary String pairs
+
+        //  000
+        //  001
+        //  010
+        //  100
+        //  101
