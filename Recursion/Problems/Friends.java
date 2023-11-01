bf939f6539be885d551441f2847d65c4566b383d
Vk4011 <maticsystem1@gmail.com>
2023-11-01 15:17:13 +0530

    fp(n-1)*(n-1)*fp(n-2);

--- Recursion/Problems/Friends.java
+++ Recursion/Problems/Friends.java
@@ -9,17 +9,18 @@ public class Friends{
         if(n==1||n==2){
             return n;
         }
-        //choice
-        //single
-        int fnm1 = fp(n-1);
+        // //choice
+        // //single
+        // int fnm1 = fp(n-1);
 
-        //pairs
-        int fnm2 = fp(n-2);
-        int pairways = (n-1)*fnm2;
+        // //pairs
+        // int fnm2 = fp(n-2);
+        // int pairways = (n-1)*fnm2;
 
-        //toways
-        int toways = fnm1+pairways;
-        return toways;
+        // //toways
+        // int toways = fnm1+pairways;
+        // return toways;
+        return fp(n-1)+(n-1)*fp(n-2);
     }
 }
 
@@ -32,4 +33,3 @@ public class Friends{
 
 
 
-        
