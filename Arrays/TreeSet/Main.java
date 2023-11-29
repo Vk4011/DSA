5cefe5bee6101114af9280207a4012c32a4ef094
Vk4011 <maticsystem1@gmail.com>
2023-11-29 13:24:30 +0530

    Tree set

---
+++ Arrays/TreeSet/Main.java
@@ -0,0 +1,25 @@
+package TreeSet;
+
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n\t Tree set \n");
+        TreeSet t=new TreeSet<>();
+        t.add(44);
+        t.add(22);
+        t.add(20);
+        t.add(30);
+        System.out.println("\n\t Format : "+t);
+
+    }
+    
+}
+
+//  Tree set
+
+
+//          Format : [20, 22, 30, 44] 
+
+
+
